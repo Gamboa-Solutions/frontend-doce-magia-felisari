de8408772ea5c3d4311b0ba482609a32969eef9d
vcpersike <vcpersike@gmail.com>
2023-10-30 19:59:13 -0300

    teste de deplaoy caprover

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{' '}
+            Doce magia{' '}
             <Image
               src="/vercel.svg"
               alt="Vercel Logo"
